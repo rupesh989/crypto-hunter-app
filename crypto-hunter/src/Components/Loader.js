67a1bab7db4ed7eaff21dec62332c1b8c577f663
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-04 22:24:51 +0530

    Create Loader.js

---
+++ crypto-hunter/src/Components/Loader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ColorRing } from "react-loader-spinner";
+const Loader = () => {
+  return (
+
+    <div style={{ display: "flex", alignItems: 'center', justifyContent: "center", height: '100vh' }}>Loading...
+
+      <ColorRing
+        visible={true}
+        height="100"
+        width="100"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+
+
+    </div>
+  )
+
+};
+
+export default Loader;
