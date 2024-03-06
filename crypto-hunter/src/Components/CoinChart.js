acb7e73505974535366c50d57e4975530c6f1a89
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-06 21:27:12 +0530

    Update CoinChart.js

--- crypto-hunter/src/Components/CoinChart.js
+++ crypto-hunter/src/Components/CoinChart.js
@@ -1,1 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Baseurl } from "./baseUrl";
+import { useParams } from "react-router-dom";
+const CoinChart = () => {
+  const [chartData, SetChartData] = useState([]);
+  const { id } = useParams();
+  const CoinChartData = async () => {
+    try {
+      const { data } = await axios.get(
+        `${Baseurl}/coins/${id}/market_chart?vs_currency=inr&days=365`
+      );
+      SetChartData(data.prices);
+      console.log(data.prices);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    CoinChartData();
+  }, []);
+  return <div>CoinChart</div>;
+};
+
+export default CoinChart;
 
